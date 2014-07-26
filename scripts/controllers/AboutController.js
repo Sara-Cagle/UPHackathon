f1f89dae8b84de6f956144a8b42f0ff777b71c00
Sara-Cagle <cagle.sara@gmail.com>
2014-07-26 11:42:35 -0500

    added about controller

---
+++ scripts/controllers/AboutController.js
@@ -0,0 +1,8 @@
+controllers.controller('AboutController', ['$scope', function($scope){
+  
+  /**
+   * The content of what goes on the page.
+  */
+  $scope.message = 'Some words here I hope?????';
+}]);
+
