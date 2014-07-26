9b5d28610ee4771526441ef5df06cb3fab92f332
Sara-Cagle <cagle.sara@gmail.com>
2014-07-26 11:33:45 -0500

    moved route provider from app into right folder

--- scripts/app.js
+++ scripts/app.js
@@ -5,21 +5,4 @@ var app = angular.module('SleepScoreApp', [
 
 var controllers = angular.module('SleepScoreApp', []);
 
-app.config(['$routeProvider', function($routeProvider){
-	$routeProvider.
-		when('/about',{
-			templateUrl: 'views/about.html',
-			controller: 'AboutController'
-		}).
-		when('/alarm',{
-			templateUrl: 'views/alarm.html',
-			controller: 'AlarmController'
-		}).
-		when('/calc',{
-			templateUrl: 'views/calc.html',
-			controller: 'CalcController'
-		}).
-		otherwise({
-			redirectTo: '/about'
-		});
-}]);
+
