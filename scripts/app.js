d76e7b20d05744836cc210138e09038edcb0a695
Sara-Cagle <cagle.sara@gmail.com>
2014-07-26 11:28:54 -0500

    routeproviding

--- scripts/app.js
+++ scripts/app.js
@@ -7,11 +7,19 @@ var controllers = angular.module('SleepScoreApp', []);
 
 app.config(['$routeProvider', function($routeProvider){
 	$routeProvider.
-		when('/someURL',{
-			templateUrl: '#',
-			controller: 'controllername'
+		when('/about',{
+			templateUrl: 'views/about.html',
+			controller: 'AboutController'
+		}).
+		when('/alarm',{
+			templateUrl: 'views/alarm.html',
+			controller: 'AlarmController'
+		}).
+		when('/calc',{
+			templateUrl: 'views/calc.html',
+			controller: 'CalcController'
 		}).
 		otherwise({
-			redirectTo: '#'
+			redirectTo: '/about'
 		});
 }]);
