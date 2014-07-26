575b207d6dee772a62380dbb28c90e5370800963
Sara-Cagle <cagle.sara@gmail.com>
2014-07-26 11:21:32 -0500

    set up routing

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,17 @@
-var app = angular.module('SleepScoreApp', []);
+var app = angular.module('SleepScoreApp', [
+	'ngRoute',
+	'controllers'
+]);
+
+var controllers = angular.module('SleepScoreApp', []);
+
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+		when('/someURL',{
+			templateUrl: '#',
+			controller: 'controllername'
+		}).
+		otherwise({
+			redirectTo: '#'
+		});
+}]);
