2cbd4471f2698e0f3b4cc0ba4a81236f861d326c
scagle <scagle6@gatech.edu>
2014-07-26 17:19:10 +0000

    added new routes

--- scripts/routeProvider.js
+++ scripts/routeProvider.js
@@ -8,11 +8,19 @@
  */
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-      when('/about', {
-	templateUrl: 'views/about.html',
-	controller: 'AboutController'
-      }).
-      otherwise({
-	redirectTo: '/about'
-      });
+    	when('/about', {
+			templateUrl: 'views/about.html',
+			controller: 'AboutController'
+      	}).
+      	when('/alarm', {
+			templateUrl: 'views/alarm.html',
+			controller: 'AlarmController'
+      	}).
+      	when('/calc', {
+			templateUrl: 'views/calc.html',
+			controller: 'CalcController'
+      	}).
+      	otherwise({
+			redirectTo: '/about'
+      	});
 }]);
